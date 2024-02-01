eadfe526f5e8bdf3b9b7f7c6a2debbc91ca8db1b
JCarpeta <98725743+USAC-LuisCorleto@users.noreply.github.com>
2024-01-31 20:34:44 -0600

    [FIX] Interfaz
    El proyecto no se encontraba creado en el repositorio, se solucionó.

---
+++ DataForge/src/main/java/DataForge/DataForge.java
@@ -0,0 +1,11 @@
+package DataForge;
+import InterfazDataForge.Interfaz;
+
+public class DataForge {
+
+    public static void main(String[] args) {
+        Interfaz menPrincipal = new Interfaz();
+        menPrincipal.setVisible(true);
+        menPrincipal.setLocationRelativeTo(null);
+    }
+}
